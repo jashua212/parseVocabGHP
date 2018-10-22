88df13c44fa667708138cc134a80421d9bfe3aee
jashua212 <jashua212@gmail.com>
2018-10-22 06:46:04 -0400

    lowercase...again

--- src/app.js
+++ src/app.js
@@ -335,11 +335,11 @@ const util = require('./appUtilities.js');
 					termTableArray.push([termObj.def]);
 
 					if (termObj.synos) {
-						termTableArray.push(['alpha:', termObj.synos]);
+						termTableArray.push(['synonyms:', termObj.synos]);
 					}
 
 					if (termObj.antos) {
-						termTableArray.push(['beta:', termObj.antos]);
+						termTableArray.push(['antonyms:', termObj.antos]);
 					}
 
 					//push termTableArray into masterTableArray
