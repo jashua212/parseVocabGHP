b0062a9aeedb491a4ebeb93a78ff951042dee672
jashua212 <jashua212@gmail.com>
2018-11-03 12:57:42 -0400

    Revert "limit appending \n to main entries"
    This reverts commit e4fb501c5fc91bb2a5f5d26e056f1a6924223bc6.

--- src/app.js
+++ src/app.js
@@ -298,17 +298,17 @@ const util = require('./appUtilities.js');
 					// if not a note -- notes have "{note on ...) " at their ends
 					if (!/\(note.+?\)\s*$/i.test(p)) {
 						// remove beginning and end quotation marks
-						var text = (p
+						var text = p
 							.replace(/^"/, '')
-							.replace(/(")(\s*\(.+\))/, '$2')) + '\n';
+							.replace(/(")(\s*\(.+\))/, '$2');
 
 						var match = text.match(/\(([^)]+:\d+)\)$/);
-						var cite = match ? match[1]: '';
+						var cite = match ? match[1] : '';
 
-						tableArray.push([text, '', cite]);
+						tableArray.push([text + '\n', '', cite + '\n']);
 
 					} else {
-						tableArray[tableArray.length - 1][1] = (p + '\n');
+						tableArray[tableArray.length - 1][1] = p + '\n';
 					}
 				});
 
