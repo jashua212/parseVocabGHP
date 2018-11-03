2514981a57f896ae242d6482e4223e5227d5f413
jashua212 <jashua212@gmail.com>
2018-11-02 22:09:23 -0400

    interim

--- src/app.js
+++ src/app.js
@@ -312,8 +312,8 @@ const util = require('./appUtilities.js');
 				});
 
 				var pageNos = tableArray.map(function (row) {
-					console.log(row);
-					var match = row[1].match(/:(\d+)\)$/);
+					// console.log(row);
+					var match = row[0].match(/:(\d+)\)$/);
 					return match ? match[1] : '';
 				});
 
